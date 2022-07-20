187c1afda2026fad1bdb1cbaac8409c4fd300ebf
joela <59021153+joela@users.noreply.github.com>
2022-07-20 13:44:42 -0300

    Cambios

--- Prolog/antecesor.pl
+++
@@ -1,9 +0,0 @@
-padre(juan,pedro).
-padre(juan,maria).
-padre(pedro,ana).
-padre(ana,nicolas).
-padre(ana,joela).
-
-antecesor(Padre,Hijo):- padre(Padre,Hijo).
-antecesor(X,Y):- padre(X,Z), antecesor(Z,Y).
-
